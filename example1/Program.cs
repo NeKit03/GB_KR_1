5c46913c065e72d3cb9c7d2bd0ad2dd327771d1c
Nikita <nikita91797@gmail.com>
2023-11-27 19:16:31 +0800

    add example1 просто выводим то что подходит нам

--- example1/Program.cs
+++ example1/Program.cs
@@ -6,11 +6,38 @@ using System;
 
 public class KR
 {
+    public static void PrintMatrix(string[] array)
+    {
+    foreach(string i in array){
+            Console.Write($"{i}, ");
+        
+      }Console.Write("\n");
+    
+    }
+
+
+    public static void SortSize(string[] array)
+    {
+        Console.WriteLine("\nМатрица с упорядоченными по колличеству символов в строках:");
+    foreach(string i in array){
+        if (i.Length<=3)
+            Console.Write($"{i}, ");
+    }
+
+        
+
+    }
 
     public static void Main()
     {
        string[] words = Console.ReadLine().Split(new char[] {' ',','}, StringSplitOptions.RemoveEmptyEntries);
 
-       
+        Console.WriteLine("Исходная строка:");
+        PrintMatrix(words);
+
+        SortSize(words);
+
+        
+        
     }
 }
